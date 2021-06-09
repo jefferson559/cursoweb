fd19cf419b116ce2910d0fa47643b505e794be35
Jefferson <jeffersonperezinfo@gmail.com>
2021-06-09 20:13:57 -0300

    09-06-2021 cambios

---
+++ Practicas/Pruebadefonts/script.js
@@ -0,0 +1,12 @@
+// let edad=prompt("Ingrese su edad");
+// console.log(edad);
+// console.log(typeof(edad))
+// edad=Number(edad)
+// console.log(typeof(edad))
+let nombres=["andres","test","test2"];
+console.log(`el lenght es:${nombres.length}`);
+nombres.push("lucas");
+console.log(`el lenght es:${nombres.length}`);
+let variable=nombres.push("pop");
+console.log(`el lenght es de:${variable}`);
+console.log(`la posicion test es :${nombres.indexOf("test.length")}`);
