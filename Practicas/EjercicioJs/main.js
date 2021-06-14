dd9286d146dee1f7960040bd0cf85712072c181d
Jefferson <jeffersonperezinfo@gmail.com>
2021-06-14 18:24:05 -0300

    cambios

---
+++ Practicas/EjercicioJs/main.js
@@ -0,0 +1,52 @@
+// let nombres=[]
+// for(let i=0;i<=5;i++){
+//     let nombre= prompt("Ingrese un nombre"); 
+//     nombres.push(nombre);
+// }
+// console.log(nombres[i])
+
+///**pares entre 10 y 20  /****////
+
+// for(let i=11;i<=21;i++){
+//     if((i%2)==0){
+//         console.log(i)
+//     } 
+// }
+
+/*////*/
+// const nombres = ["Juan", "Pedro", "Wally", "Sofía", "Amelia", "Wally"];
+// for(let i=0;i<nombres.length;i++){
+//     if("Wally" === nombres[i]){
+//         console.log(`Wally esta en la pos:${i}`)
+//     }
+// }
+
+// const lista=[2,3,4,5,6,7,8]
+//   for(let i=0;i<=lista.length;i++){
+//       if((lista[i]%2)==0){
+//         console.log(`El numero:${lista[i]} es par`)
+//       } 
+//   }
+
+
+  ///*Destino mas populares///
+//let DestinosPopulares=["ecuador",]
+
+let posteo={id:0,titulo:'aa',autor:'aa',categoria:'aaaaa'}
+let posteo2={id:1,titulo:'aaaa',autor:'aaaa',categoria:'aaaaa'}
+let posteo3={id:2,titulo:'aaaaa',autor:'aaaaaa',categoria:'aaaaa'}
+let array=[posteo,posteo2,posteo3]
+
+for(let p in posteo)
+{
+    console.log(p)
+}
+for(let p in posteo)
+{
+    console.log(posteo[p])
+}
+
+ for(let a in array)
+{
+    console.log(a)
+} 
